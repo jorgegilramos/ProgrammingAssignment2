7e9ea38f179d22719ba8dbbab5fa3efea81a7fe5
jorgegilramos <jorgegilramos@users.noreply.github.com>
2014-04-25 19:07:06 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -15,48 +15,51 @@
 # Create the wrapper makeCacheMatrix function which encapsulate the list of methods defined
 # to be used in the cacheSolve function (set, get, setinverse and getinverse)
 makeCacheMatrix <- function(x = matrix()) {
-  # Checking some errors
-  if(!is.matrix(x)){
-    stop("matrix expected")
-  }
-  if(nrow(x) != ncol(x)){ 
-    stop("only square matrices can be inverted")
-  }
+    # Checking some errors
+    if(!is.matrix(x)){
+        stop("matrix expected")
+    }
+    if(nrow(x) != ncol(x)){ 
+      stop("only square matrices can be inverted")
+    }
   
-  inverseMatrix <- NULL
-  set <- function(newMatrix) {
-    x <<- newMatrix
-    inverseMatrix <<- NULL
-  }
+    inverseMatrix <- NULL
+    set <- function(newMatrix) {
+      x <<- newMatrix
+      inverseMatrix <<- NULL
+    }
   
-  get <- function() x
-  setinverse <- function(inverse) inverseMatrix <<- inverse
-  getinverse <- function() inverseMatrix
-  # Return the list of methods
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+    get <- function() x
+    setinverse <- function(inverse) inverseMatrix <<- inverse
+    getinverse <- function() inverseMatrix
+    # Return the list of methods
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 # Solve the inverse of a square matrix
 cacheSolve <- function(x, ...) {
-  # Checking if the inverse matrix is in cache
-  inverseMatrix <- x$getinverse()
-  if(!is.null(inverseMatrix)) {
-    message("getting cached inverse matrix")
-    # return the cached matrix
-    return(inverseMatrix)
-  }
+    # Checking if the inverse matrix is in cache
+    inverseMatrix <- x$getinverse()
+    if(!is.null(inverseMatrix)) {
+      message("getting cached inverse matrix")
+      # Return the cached matrix
+      return(inverseMatrix)
+    }
   
-  # Calculate the inverse matrix (usually a long time operation ...)
-  data <- x$get()
-  inverseMatrix <- solve(data, ...)
-  x$setinverse(inverseMatrix)
+    # Calculate the inverse matrix (usually a long time operation ...)
+    data <- x$get()
+    inverseMatrix <- solve(data, ...)
+    x$setinverse(inverseMatrix)
   
-  # Return the calculated inverse
-  inverseMatrix
+    # Return the calculated inverse
+    inverseMatrix
 }
 
+# Checking errors
+#makeCacheMatrix(c(1))                                   # Error: matrix expected
+#makeCacheMatrix(matrix(c(1,2,3,4,5,6), nrow=2, ncol=3)) # Error: only square matrices
 
 # Practical example
 mdat <- matrix(c(1, 2, 11, 12), nrow = 2, ncol = 2)
@@ -64,6 +67,3 @@ matrix1 <- makeCacheMatrix(mdat)
 cacheSolve(matrix1)
 cacheSolve(matrix1)
 
-# Checking errors
-makeCacheMatrix(c(1))                                   # Error: matrix expected
-makeCacheMatrix(matrix(c(1,2,3,4,5,6), nrow=2, ncol=3)) # Error: only square matrices
